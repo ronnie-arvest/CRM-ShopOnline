c5352ec14179ec28f893e1f02e7e37f623364620
ronnie-arvest <melikov96@gmail.com>
2023-07-29 00:47:47 +0300

    add table

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 
 const title = 'Холодильник Candy';
 console.log('Наименование товара: ', title);
-  
+
 const count = 5;
-  
+
 const category = 'Холодильники и морозильные камеры';
-  
+
 const price = 29990;
 
 console.log('Общая сумма товара:', count * price);
+
