aaec44c0167a944025844475038aebec034ab8c2
ronnie-arvest <melikov96@gmail.com>
2023-11-13 20:35:36 +0300

    finalworkk

--- js/game.js
+++ js/game.js
@@ -1,61 +1,84 @@
-'use strict';
+// Модуль игры
+const gameModule = (function () {
+  let playerBalls = 5;
+  let botBalls = 5;
 
-const startGame = (function () {
-  let userScore = 0;
-  let computerScore = 0;
-
-  function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  function isEven(num) {
+      return num % 2 === 0;
   }
 
-  function getComputerMove() {
-    const moves = ['камень', 'ножницы', 'бумага'];
-    const randomIndex = getRandomIntInclusive(0, 2);
-    return moves[randomIndex];
+  function getRandomInt(min, max) {
+      return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  function getRoundWinner(userMove, computerMove) {
-    if (!userMove || !['камень', 'ножницы', 'бумага'].includes(userMove.toLowerCase())) {
-      return 'Некорректный ввод пользователя';
-    }
+  function makeMove() {
+      // Ход игрока
+      let playerGuess = prompt(`У вас ${playerBalls} шариков. Введите число от 1 до ${playerBalls} или нажмите "Отмена" для завершения:`);
 
-    if (userMove === computerMove) {
-      return 'Ничья';
-    } else if (
-      (userMove === 'камень' && computerMove === 'ножницы') ||
-      (userMove === 'ножницы' && computerMove === 'бумага') ||
-      (userMove === 'бумага' && computerMove === 'камень')
-    ) {
-      userScore++;
-      return 'Вы победили!';
-    } else {
-      computerScore++;
-      return 'Победил компьютер';
-    }
-  }
-
-  function game() {
-    const userMove = prompt('Камень, ножницы или бумага?');
-
-    if (userMove === null) {
-      const confirmExit = confirm('Хотите выйти?');
-      if (confirmExit) {
-        alert(`Ваши очки: ${userScore}\nОчки компьютера: ${computerScore}`);
-        return;
+      // Проверка ввода игрока и обработка отмены
+      if (playerGuess === null) {
+          console.log("Игра завершена. Результат:");
+          console.log(`У вас осталось ${playerBalls} шариков. У бота ${botBalls} шариков.`);
+          return;
       }
-    }
 
-    const computerMove = getComputerMove();
+      playerGuess = parseInt(playerGuess);
 
-    const roundResult = getRoundWinner(userMove, computerMove);
-    alert(`Вы: ${userMove}\nКомпьютер: ${computerMove}\n${roundResult}`);
+      // Проверка ввода игрока
+      while (isNaN(playerGuess) || playerGuess < 1 || playerGuess > playerBalls) {
+          playerGuess = prompt(`Неверный ввод. Введите число от 1 до ${playerBalls} или нажмите "Отмена" для завершения:`);
 
-    game();
+          // Проверка отмены
+          if (playerGuess === null) {
+              console.log("Игра завершена. Результат:");
+              console.log(`У вас осталось ${playerBalls} шариков. У бота ${botBalls} шариков.`);
+              return;
+          }
+
+          playerGuess = parseInt(playerGuess);
+      }
+
+      // Ход бота
+      let botGuess = getRandomInt(1, botBalls);
+
+      // Определение, угадал ли бот
+      if (isEven(playerGuess) === isEven(botGuess)) {
+          // Бот угадал
+          console.log(`Бот угадал! Берет ${botGuess} шариков.`);
+          botBalls += botGuess;
+          playerBalls -= playerGuess;
+      } else {
+          // Бот не угадал
+          console.log(`Бот не угадал. Вы берете ${playerGuess} шариков.`);
+          botBalls -= botGuess;
+          playerBalls += playerGuess;
+      }
+
+      // Вывод текущего состояния игры
+      console.log(`У вас осталось ${playerBalls} шариков. У бота ${botBalls} шариков.`);
+
+      // Проверка на окончание игры
+      if (playerBalls <= 0) {
+          console.log("Вы проиграли! У вас закончились шарики.");
+      } else if (botBalls <= 0) {
+          console.log("Вы выиграли! У бота закончились шарики.");
+      } else {
+          // Продолжение игры
+          makeMove();
+      }
   }
 
+  // Начало игры
+  function startGame() {
+      console.log("Добро пожаловать в игру!");
+      makeMove();
+  }
+
+  // Возвращаем метод для начала игры
   return {
-    game: game,
+      startGame: startGame,
   };
 })();
+
+// Запуск игры
+gameModule.startGame();
