15e3f81f3ec6195468852acd6a9b423739613ea8
ronnie-arvest <melikov96@gmail.com>
2023-11-18 01:09:55 +0300

    add js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const modal = document.getElementById('popup');
+modal.style.display = 'flex';
+
+const modalTitle = document.querySelector('.product__title');
+const modalId = document.querySelector('.product__id');
+const modalForm = document.querySelector('.product__label-container');
+const modalCheckBox = document.querySelector('.product__checkbox-input');
+const modalInputCheckBox = document.querySelector('.product__input-desc');
+const modalTotalPrice = document.querySelector('.product__input');
