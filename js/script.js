72adf29d58006d89c311ce324e386853e5747a71
ronnie-arvest <melikov96@gmail.com>
2023-11-28 14:35:35 +0300

    add js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 'use strict';
 
+{
 const popup = document.querySelector('#popup');
 const popupBody = document.querySelector('.product__body');
 const openBtn = document.querySelector('.main-content__button');
 const closePopup = document.querySelector('.product__close');
 const overLay = document.querySelector('.main');
+const delBtn = document.querySelector('.main-content__tdd-delete');
+const tBody = document.querySelector('.main-content__tbody')
+const imgTd = document.querySelector('.main-content__tdd-noimg');
 
 openBtn.addEventListener('click', () => {
   popup.style.display = 'block';
@@ -14,12 +18,23 @@ closePopup.addEventListener('click', () => {
   popup.style.display = 'none';
 });
 
-popup.addEventListener('click', event => {
-  event.stopPropagation();
-});
-
 popup.addEventListener('click', (event) => {
   if (!popupBody.contains(event.target)) {
     popup.style.display = 'none';
   }
 });
+
+delBtn.addEventListener('click', () => {
+  const tableRow = document.querySelectorAll('.main-content__trr');
+    tableRow.style.display = 'none';
+  });
+
+tBody.addEventListener('click', e => {
+  const target = e.target;
+  if (target.closest('.main-content__tdd-delete')) {
+    const rowToDelete = target.closest('.main-content__trr');
+    rowToDelete.remove();
+  };
+});
+
+}
